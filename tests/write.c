9669b86042044c0ae109a4f59bcabbc6b1d50970
Nathaniel McCallum <npmccallum@redhat.com>
2011-06-23 19:38:31 -0400

    [tests/write] refactor to avoid race conditions

--- tests/write.c
+++ tests/write.c
@@ -35,92 +35,68 @@
 #define DATA "hello"
 #define DATALEN 5
 
-static pid_t pid;
-static int count;
+static int fds[2];
 
 static void
-error_cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
+timeout_cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
 {
     printf("ERROR: Timeout!\n");
+    if (fds[0] >= 0)
+        close(fds[0]);
+    if (fds[1] >= 0)
+        close(fds[1]);
+
     retval = 1;
     verto_break(ctx);
 }
 
 static void
-cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
+error_cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
 {
     int fd = verto_get_fd(ev);
+    assert(fd == fds[1]);
 
-    count++;
-    if (write(fd, DATA, DATALEN) != DATALEN) {
-        if (count != 2) {
-            printf("ERROR: %d: %s\n", errno, strerror(errno));
-            retval = 1;
-        }
+    /* When we get here, the fd should be closed, so an error should occur */
+    assert(write(fd, DATA, DATALEN) != DATALEN);
+    verto_break(ctx);
+    close(fd);
+    fds[1] = -1;
+}
 
-        goto out;
-    }
+static void
+read_cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
+{
+    unsigned char buff[DATALEN];
+    int fd = verto_get_fd(ev);
+    assert(fd == fds[0]);
 
-    if (count > 1) {
-        printf("ERROR: Second call successful!?\n");
-        retval = 1;
-        goto out;
-    }
+    assert(read(fd, buff, DATALEN) == DATALEN);
+    assert(verto_add_write(ctx, VERTO_EV_PRIORITY_HIGH, error_cb, NULL, fds[1]));
+    close(fd);
+    fds[0] = -1;
+}
 
-    usleep(200000); /* 0.2 seconds; make time for child to close() */
-    verto_repeat(ev);
-    return;
+static void
+write_cb(struct vertoEvCtx *ctx, struct vertoEv *ev)
+{
+    int fd = verto_get_fd(ev);
+    assert(fd == fds[1]);
 
-    out:
-        waitpid(pid, NULL, 0);
-        close(fd);
-        verto_break(ctx);
-        return;
+    assert(write(fd, DATA, DATALEN) == DATALEN);
 }
 
 int
 do_test(struct vertoEvCtx *ctx)
 {
-    unsigned char buff[DATALEN];
-    int fds[2] = {-1, -1};
-    pid = 0;
-    count = 0;
+    fds[0] = -1;
+    fds[1] = -1;
 
-    /* Ignore when the pipe breaks */
     if (!verto_add_signal(ctx, VERTO_EV_PRIORITY_DEFAULT, VERTO_SIG_IGN, NULL, SIGPIPE))
         signal(SIGPIPE, SIG_IGN);
 
-    if (pipe(fds) != 0)
-        return 1;
-
-    if (!verto_add_write(ctx, VERTO_EV_PRIORITY_DEFAULT, cb, NULL, fds[1])) {
-        printf("ERROR: Unable to add write call!\n");
-        close(fds[0]);
-        close(fds[1]);
-        return 1;
-    }
-
-    if (!verto_add_timeout(ctx, VERTO_EV_PRIORITY_DEFAULT, error_cb, NULL, 1000)) {
-        printf("ERROR: Unable to add timeout!\n");
-        close(fds[0]);
-        close(fds[1]);
-        return 1;
-    }
-
-    pid = fork();
-    if (pid < 0)
-        return 1;
-    else if (pid == 0) {
-        close(fds[1]);
-        usleep(100000); /* 0.1 seconds */
-        if (read(fds[0], buff, DATALEN) != DATALEN) {
-            close(fds[0]);
-            exit(1);
-        }
-        close(fds[0]);
-        exit(0);
-    }
-    close(fds[0]);
-
+    assert(pipe(fds) == 0);
+    assert(verto_add_timeout(ctx, VERTO_EV_PRIORITY_LOW, timeout_cb, NULL, 1000));
+    assert(verto_add_write(ctx, VERTO_EV_PRIORITY_HIGH, write_cb, NULL, fds[1]));
+    assert(verto_add_read(ctx, VERTO_EV_PRIORITY_LOW, read_cb, NULL, fds[0]));
     return 0;
 }
